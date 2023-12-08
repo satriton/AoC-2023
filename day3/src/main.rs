ccc16723ebee5b0e55da3aeff215e766bf32dd01
Paul Anaclet <paul.anaclet-ext@ixxitechside.com>
2023-12-08 10:32:45 +0100

    feat: wip

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,23 +1,39 @@
-use std::{fs::read_to_string, collections::HashMap};
+use std::{fs::read_to_string, ops::Range};
+
+#[derive(Debug, PartialEq)]
+struct SchematicNumber {
+    number: u16,
+    range: Range<u8>,
+}
+
 
 fn main() {
-    let lines: Vec<String> = read_to_string("src/input.txt") 
+    let lines: Vec<String> = read_to_string("src/exemple.txt") 
         .unwrap()
         .lines()
         .map(String::from)
         .collect();
 
-    
 
     println!("");
 }
 
+fn extract_schematic_number_from_line(string: String) -> Vec<SchematicNumber>{
+    for char in string.chars() {
+        
+    }
+    Vec::new()
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{SchematicNumber, extract_schematic_number_from_line};
+
 
     #[test]
-    fn should_extract_number_from_line() {
-        let input = "1abc2".to_string();
-        assert_eq!(12, 12);
+    fn should_extract_schematic_number_from_line() {
+        let expected = vec![SchematicNumber {number: 467, range: 0..2}, SchematicNumber {number: 114, range: 5..6}];
+        let input = "467..11...".to_string();
+        assert_eq!(extract_schematic_number_from_line(input), expected);
     }
 }
